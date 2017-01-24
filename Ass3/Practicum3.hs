e6e5dab593384f5b6588e2977f7b8581174b4f16
dda410 <dimitri1215@gmail.com>
2017-01-24 21:48:10 +0100

    till combinatory logix included

--- Ass3/Practicum3.hs
+++ Ass3/Practicum3.hs
@@ -39,14 +39,54 @@ showterm I = "I"
 showterm (App t1 t2) = "(" ++ (showterm t1) ++ (showterm t2) ++ ")"
 
 isredex :: Term -> Bool
-isredex = undefined
+isredex (App t1 S) = True
+isredex (App t1 K) = True
+isredex (App t1 I) = True
+isredex (App t1 t2) = False
 
 isnormalform :: Term -> Bool
-isnormalform = undefined
+isnormalform I = True
+isnormalform K = True
+isnormalform S = True
+isnormalform (App t1 t2) = isnormalform' t1 && isnormalform' t2
+isnormalform' I = True
+isnormalform' K = True
+isnormalform' S = True
+isnormalform' (App t1 t2) = False
 
 outerstep :: Term -> Term
-outerstep = undefined
+outerstep (App t1 t2) =
+  if isredex (App t1 t2)
+  then dostep (findstep (App t1 t2)) (App t1 t2) 
+  else (App t1 t2)
+  
+-- finds the Cl-term to apply the rule from
+findstep (App t1 t2) =
+  if isnormalform (App t1 t2)
+  then t1
+  else findstep t1
 
+-- dostep performs one step according to the Cl-terms rules
+dostep I (App t1 t2) = resolveI (App t1 t2) 
+dostep K (App t1 t2) = resolveK (App t1 t2)
+dostep S (App t1 t2) = resolveS (App t1 t2)
+resolveI (App I t2) = t2
+resolveI (App t1 t2) = (App (resolveI t1) t2)
+resolveK (App K t2) = t2
+resolveK (App t1 t2) =
+  if isnormalform t1
+  then (resolveK t1)
+  else (App (resolveK t1) t2)
+resolveS (App t1 t2) =
+  if isnormalform (returnT1(t1))
+  then App (App (returnT2(returnT1(t1))) t2) (App (returnT2(t1)) t2)
+  else (App (resolveS t1) t2)
+
+-- returns the first term
+returnT1 (App t1 t2) = t1
+-- returns the second term
+returnT2 (App t1 t2) = t2
+  
 -- Exercise  3
 data Thing = Undefined1
   deriving (Show, Eq, Bounded, Enum)
